28cd4eaf53593c8aa6af19ef55e4944db870be23
Fieldbishop <74423885+Fieldbishop@users.noreply.github.com>
2022-01-27 13:45:10 +0200

    Added some product/order functionality

---
+++ src/main/java/model/classes/Product.java
@@ -0,0 +1,48 @@
+package model.classes;
+
+public class Product{
+    //private int id;
+    private String name;
+    private String description;
+    private float price;
+    private int stock;
+
+    public Product(String name, String description, float price, int stock) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
--- src/main/java/model/interfaces/IOrder.java
+++ src/main/java/model/interfaces/IOrder.java
@@ -1,4 +1,13 @@
 package model.interfaces;
 
+import model.classes.Product;
+
 public interface IOrder {
+
+    /**
+     * Adds a new product to an order.
+     * @param x Scanned product
+     * @return true if success
+     */
+    boolean addProductToOrder(Product x);
 }
