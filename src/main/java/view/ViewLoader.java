df7dad73c03cc68ce907a0a6e60fcf49b4cda7f9
nicklasu <nicklass84@gmail.com>
2022-01-28 12:12:34 +0200

    Refactored code regarding view switching. The code now resides in ViewLoader.java and gets called every time a view has to be switched.

---
+++ src/main/java/view/ViewLoader.java
@@ -0,0 +1,11 @@
+package view;
+
+import javafx.scene.layout.AnchorPane;
+
+public class ViewLoader {
+    public ViewLoader(AnchorPane aPaneParent, AnchorPane aPaneChild){
+        aPaneParent.getChildren().setAll(aPaneChild);
+        aPaneChild.prefWidthProperty().bind(aPaneParent.widthProperty());
+        aPaneChild.prefHeightProperty().bind(aPaneParent.heightProperty());
+    }
+}
