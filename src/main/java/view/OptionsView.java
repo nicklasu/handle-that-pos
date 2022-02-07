6dedf6203846caef2653d64679ef882d5d40d9f5
Samu Luoma <Samu.Luoma@metropolia.fi>
2022-02-07 16:18:39 +0200

    first and lastname can now be seen in options view

--- src/main/java/view/OptionsView.java
+++ src/main/java/view/OptionsView.java
@@ -3,14 +3,21 @@ package view;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import model.classes.Product;
+import model.classes.User;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class OptionsView {
     private MainApp mainApp;
+    @FXML
+    private Text fName;
+    @FXML
+    private Text lName;
     @FXML
     private AnchorPane transactionAnchorPane;
     public void loadMainView(ActionEvent event) throws IOException {
@@ -20,7 +27,12 @@ public class OptionsView {
     }
 
     public void setMainApp(MainApp mainApp) {
+
         this.mainApp = mainApp;
+        User user = this.mainApp.getEngine().getUser();
+        fName.setText(user.getfName());
+        lName.setText(user.getlName());
+
         if (this.mainApp.getEngine().getTransaction() != null) {
         }
     }
