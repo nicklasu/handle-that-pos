93ab6100a80a0e214f831d8e04e729906ce64dc9
Samu Luoma <Samu.Luoma@metropolia.fi>
2022-03-02 11:30:25 +0200

    hotfix

--- src/test/java/POS/ProductTest.java
+++ src/test/java/POS/ProductTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class ProductTest extends TestParent {
     Product test;
 
-    public ProductTest(Product test) {
+    public ProductTest() {
     }
 
     @BeforeAll
