701672bd143b2370086ef67f378bd23a987768c2
Samu Luoma <Samu.Luoma@metropolia.fi>
2022-03-10 17:10:02 +0200

    attempt to make hwid more unique

--- src/main/java/model/classes/HWID.java
+++ src/main/java/model/classes/HWID.java
@@ -8,7 +8,7 @@ public class HWID {
      */
     public static String getHWID() {
         try {
-            String toEncrypt = System.getenv("COMPUTERNAME") + System.getProperty("user.name") + System.getenv("PROCESSOR_IDENTIFIER") + System.getenv("PROCESSOR_LEVEL");
+            String toEncrypt = System.getenv("COMPUTERNAME") + System.getProperty("user.name") + System.getenv("PROCESSOR_IDENTIFIER") + System.getenv("PROCESSOR_LEVEL")+ System.getenv("PROCESSOR_REVISION") + System.getenv("NUMBER_OF_PROCESSORS") + System.getenv("SystemDrive");
             MessageDigest md = MessageDigest.getInstance("MD5");
             md.update(toEncrypt.getBytes());
             StringBuffer hexString = new StringBuffer();
