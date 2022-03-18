3c98e1297e3f0e5328fafaaffb69ee12726ca609
annarae <anna.raevskaia@metropolia.fi>
2022-03-18 10:11:08 +0200

    Fix.

--- src/main/java/view/UserManagementView.java
+++ src/main/java/view/UserManagementView.java
@@ -38,26 +38,27 @@ public class UserManagementView {
         }
         wrapperPaneUsers.getChildren().add(newLoadedPane);
     }
-//        /**
-//         * Switch to delete-user-view.fxml
-//         */
-//        public void deleteUserPane(ActionEvent event) throws IOException {
-//
-//            System.out.println("delete user button was pressed");
-//            wrapperPaneUsers.getChildren().clear();
-//            Pane newLoadedPane = null;
-//            try {
-//                this.loader = new FXMLLoader();
-//                this.loader.setLocation(getClass().getResource("delete-user-view.fxml"));
-//                newLoadedPane = this.loader.load();
-//                AddUserView view = this.loader.getController();
-//                view.setMainApp(mainApp);
-//                //newLoadedPane3 = FXMLLoader.load(getClass().getResource("delete-user-view.fxml"));
-//            } catch (IOException ex) {
-//                ex.printStackTrace();
-//            }
-//            wrapperPaneUsers.getChildren().add(newLoadedPane);
-//        }
+
+        /**
+         * Switch to delete-user-view.fxml
+         */
+        public void deleteUserPane(ActionEvent event) throws IOException {
+
+            System.out.println("delete user button was pressed");
+            wrapperPaneUsers.getChildren().clear();
+            Pane newLoadedPane = null;
+            try {
+                this.loader = new FXMLLoader();
+                this.loader.setLocation(getClass().getResource("delete-user-view.fxml"));
+                newLoadedPane = this.loader.load();
+                //AddUserView view = this.loader.getController();
+                //view.setMainApp(mainApp);
+                //newLoadedPane3 = FXMLLoader.load(getClass().getResource("delete-user-view.fxml"));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            wrapperPaneUsers.getChildren().add(newLoadedPane);
+        }
 
         /**
          * Switch to edit-user-view.fxml
