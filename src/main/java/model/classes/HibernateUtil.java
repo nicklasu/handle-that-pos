90fc70d39c7b8b10bddf18ac7cb0bb745d201641
Fieldbishop <74423885+Fieldbishop@users.noreply.github.com>
2022-03-25 12:32:32 +0200

    Javadoccing

--- src/main/java/model/classes/HibernateUtil.java
+++ src/main/java/model/classes/HibernateUtil.java
@@ -3,13 +3,26 @@ package model.classes;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+/**
+ * Helper class for accessing database with hibernate
+ * @author Nicklas Sundell, Anna Raevskaia, Lassi Piispanen, Antti Taponen and Samu Luoma
+ */
 public class HibernateUtil {
 
+    /**
+     * Deals with creating the sessions to connect to database
+     */
     public static SessionFactory factory;
 
+    /**
+     * Empty constructor required for hibernate
+     */
     private HibernateUtil() {
     }
 
+    /**
+     * @return instance of SessionFactory
+     */
     public static synchronized SessionFactory getSessionFactory() {
 
         if (factory == null) {
--- src/main/java/model/classes/OrderProduct.java
+++ src/main/java/model/classes/OrderProduct.java
@@ -2,48 +2,86 @@ package model.classes;
 
 import javax.persistence.*;
 
+/**
+ * Object representing a join table mapping products to orders in the database
+ * @author Nicklas Sundell, Anna Raevskaia, Lassi Piispanen, Antti Taponen and Samu Luoma
+ */
 @Entity
 @Table(name = "Sisältyy")
 @AssociationOverrides({
         @AssociationOverride(name = "primaryKey.product", joinColumns = @JoinColumn(name = "TuoteID")),
         @AssociationOverride(name = "primaryKey.order", joinColumns = @JoinColumn(name = "TilausID"))})
 public class OrderProduct {
+    /**
+     * Order identifier for mapping products to
+     */
     private OrderProductId primaryKey = new OrderProductId();
 
+    /**
+     * Amount of a product in the order
+     */
     private int amount = 1;
 
+    /**
+     * @return order identifier
+     */
     @EmbeddedId
     public OrderProductId getPrimaryKey() {
         return primaryKey;
     }
 
+    /**
+     * Sets order identifier
+     * @param primaryKey order identifier
+     */
     public void setPrimaryKey(OrderProductId primaryKey) {
         this.primaryKey = primaryKey;
     }
 
+    /**
+     * @return the order
+     */
     @Transient
     public Order getOrder() {
         return getPrimaryKey().getOrder();
     }
 
+    /**
+     * Sets order
+     * @param order the order
+     */
     public void setOrder(Order order) {
         getPrimaryKey().setOrder(order);
     }
 
+    /**
+     * @return a product
+     */
     @Transient
     public Product getProduct() {
         return getPrimaryKey().getProduct();
     }
 
+    /**
+     * Sets a product for a identifier
+     * @param product
+     */
     public void setProduct(Product product) {
         getPrimaryKey().setProduct(product);
     }
 
+    /**
+     * @return amount of a product
+     */
     @Column(name = "Määrä")
     public int getAmount() {
         return amount;
     }
 
+    /**
+     * Sets amount of a product
+     * @param amount
+     */
     public void setAmount(int amount) {
         this.amount = amount;
     }
