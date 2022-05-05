9486bf747854ab7f9a4fee299185cc86c9ca9117
samulu <Samu.Luoma@metropolia.fi>
2022-05-05 11:44:57 +0300

    comments

--- src/test/java/POS/HWIDTest.java
+++ src/test/java/POS/HWIDTest.java
@@ -7,13 +7,22 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+/**
+ * HWIDTest
+ */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class HWIDTest extends TestParent{
+    /**
+     * Before all tests, do a system printout.
+     */
     @BeforeAll
     public void beforeAll() {
         System.out.println("HWID tests...");
     }
 
+    /**
+     * Test of which operating system is being used.
+     */
     @Test
     public void osTest(){
         String os = System.getProperty("os.name");
