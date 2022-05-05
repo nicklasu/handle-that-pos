e57248aaa07e3281b70f13eec4a3b1ca7803adff
samulu <Samu.Luoma@metropolia.fi>
2022-05-05 11:50:05 +0300

    comments

--- src/test/java/POS/CustomerTest.java
+++ src/test/java/POS/CustomerTest.java
@@ -3,6 +3,9 @@ package POS;
 import model.classes.Customer;
 import org.junit.jupiter.api.*;
 
+/**
+ * This class tests the Customer class.
+ */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CustomerTest extends TestParent {
     Customer customer;
@@ -10,33 +13,51 @@ public class CustomerTest extends TestParent {
     public CustomerTest() {
     }
 
+    /**
+     * Before all tests, print a line to indicate the start of the test
+     */
     @BeforeAll
     public void beforeAll() {
         System.out.println("Customer test...");
     }
 
+    /**
+     * Test for creating a new customer with customer level index 0
+     */
     @BeforeEach
     public void setUp() {
         this.customer = new Customer(0);
     }
 
+    /**
+     * Test of setId method, of class Customer.
+     */
     @Test
     public void setId() {
         customer.setId(1);
         Assertions.assertEquals(1, this.customer.getId(), "Setting ID does not work");
     }
 
+    /**
+     * Test of getId method, of class Customer.
+     */
     @Test
     public void getId() {
         customer.setId(0);
         Assertions.assertEquals(0, this.customer.getId(), "Getting ID does not work");
     }
 
+    /**
+     * Test of getCustomerLevelIndex method, of class Customer.
+     */
     @Test
     public void getCustomerLevelIndex() {
         Assertions.assertEquals(0, this.customer.getCustomerLevelIndex(), "getting CustomerLevelIndex does not work");
     }
 
+    /**
+     * Test of toStringTest method, of class Customer.
+     */
     @Test
     public void toStringTest() {
         System.out.println(customer.toString());
